44654aaf79090f844af2eaa7d91baa7123593c9d
DebdutChowdhury <debdut.chowdhury2018@gmail.com>
2021-03-01 21:41:20 +0530

    Here create a cab invoice generator

---
+++ src/main/java/bridgeLabz/RideRepository.java
@@ -0,0 +1,19 @@
+package bridgeLabz;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RideRepository {
+    Map<String, Ride[]> userRides = new HashMap<>();
+
+
+    public void addRideForUser(String userId, Ride[] rides)  {
+
+        userRides.put(userId, rides);
+    }
+
+
+    public Ride[] getRidesForUser(String userId) {
+        return userRides.get(userId);
+    }
+}
